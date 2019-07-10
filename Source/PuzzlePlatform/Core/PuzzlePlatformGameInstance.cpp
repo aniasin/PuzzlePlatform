73fc9d9cd6acd8f455abd131bed9330fb58da2de
Sillik1 <pierrat.laurent@gmail.com>
2019-07-10 19:49:59 +0200

    Hosting and joining session

--- Source/PuzzlePlatform/Core/PuzzlePlatformGameInstance.cpp
+++ Source/PuzzlePlatform/Core/PuzzlePlatformGameInstance.cpp
@@ -2,4 +2,38 @@
 
 
 #include "PuzzlePlatformGameInstance.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/Engine.h"
 
+UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer & ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("GAME INSTANCE CONTRUCTOR"))
+}
+
+void UPuzzlePlatformGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GAME INSTANCE INIT"))
+}
+
+void UPuzzlePlatformGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine)) { return; }
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting!"));
+
+	UWorld* World = GetWorld(); 
+	if(!ensure(World)) { return; }
+	World->ServerTravel("/Game/Maps/Level_01?Listen");
+}
+
+void UPuzzlePlatformGameInstance::Join(const FString Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine)) { return; }
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Joining: %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController)) { return; }
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
